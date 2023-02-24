2352617dad7beef5cc147433ca2210315e5d092b
kiseigod <2463675414@qq.com>
2023-02-24 08:31:20 +0800

    first commit

---
+++ 单文件组件/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>学校名称:{{schoolName}}</h2>
+        <h2>学校地址:{{address}}</h2>
+        <button @click="showName">点击显示名称</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data() {
+        return {
+            schoolName: '武大',
+            address:'武汉'
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.schoolName)
+        }
+    },
+}
+</script>
